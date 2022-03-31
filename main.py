1dc0a28a59d6e9d07e2c7a43e09f03578ece0040
Zach Snyder <sadin@fedoraproject.org>
2022-03-31 09:02:41 -0400

    init pre-commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import sys
+
+if __name__ == '__main__':
+    print(f'{sys.argv[1]}')
+    print(f'{sys.argv[2]}')
+    # for i, arg in enumerate(sys.argv):
+    #    print(f"Argument {i:>6}: {arg}")
