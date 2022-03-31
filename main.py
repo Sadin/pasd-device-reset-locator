72c9617488696690a8a4894c89731f5597c4a4c0
Zach Snyder <sadin@fedoraproject.org>
2022-03-31 09:47:14 -0400

    check for correct # of params

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from __future__ import annotations
 
 import sys
 
+import pandas as pd
+
 if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        raise ValueError(
+            'application requires 2 filepaths for input,' +
+            'Prime CSV path followed by Intune CSV path.')
+
     primePath = sys.argv[1]
     intunePath = sys.argv[2]
+
+    primeData = pd.read_csv(primePath)
+    intuneDada = pd.read_csv(intunePath)
 
     print(f'Prime(wireless): {primePath}')
     print(f'Endpoint(Intune): {intunePath}')
